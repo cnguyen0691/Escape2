416b4ac7196ae04d9712a9c478fca9786bbda690
cnguyen0691 <chaunguyen0691@gmail.com>
2019-04-17 16:40:43 -0400

    escape2

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Cost\t" + "Quantity\t" +"Total\t" );
+        System.out.println("$1,000.00\t" + "4\t" +"  $4,000.00\t" );
+        System.out.println("$   50.00\t" + "8\t" +"    $400.00\t" );
+        System.out.println("______________________________" );
+        System.out.println("TOTAL:\t" + "   12\t" +"    $4,400.00\t" );
+    }
+}
